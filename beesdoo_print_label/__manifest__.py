7ef99aaecb8e29e1dd759b4f65202a669514e4f9
matdubuisson <matteo.dubuisson@student.uclouvain.be>
2022-07-01 14:52:19 +0200

    Add 3 modules of Polln group

--- beesdoo_print_label/__manifest__.py
+++ beesdoo_print_label/__manifest__.py
@@ -3,19 +3,37 @@
 
 {
     "name": "Beesdoo Print Label",
-    "summary": """Product labels""",
+
+    "author": "Polln group",
+
+    "summary": """
+    Bring changes at the pdf production of beesdoo_print_category
+    """,
+
+    "description": """
+    """,
+
     "version": "12.0.1.1.2",
+
     "license": "AGPL-3",
+
     "category": "Sales",
+
     "author": "Coop IT Easy SCRLfs",
+
     "website": "https://github.com/beescoop/Obeesdoo",
+
     "depends": [
         "beesdoo_product",
         "product_print_category",
     ],
+
     "data": [
         "views/report_pricetag_normal.xml",
         "data/product_print_category.xml",
     ],
+
     "installable": True,
+    "application": True,
+    "auto_install": False
 }
--- stock_move_view_order/__manifest__.py
+++ stock_move_view_order/__manifest__.py
@@ -1,40 +1,35 @@
-# -*- coding: utf-8 -*-
+#coding:utf-8
 {
     'name': "stock_move_view_order",
 
     'author': 'Polln group',
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    Module that reverses the order in function of dates in products
+    """,
 
     'website': "https://github.com/beescoop/Obeesdo",
     'licence': "AGPL-3",
 
     'description': """
-        Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'website': "https://github.com/beescoop/Obeesdoo",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Extra tools', # HERE
+    'category': 'Extra tools',
     'version': '12.0.2.1.0',
 
     'sequence': '10',
 
-    # any module necessary for this one to work correctly
     'depends': ['base', 'stock'],
 
-    # always loaded
     'data': [
         "views/inhereted_view_from_stock_move_line.xml"
-        # 'security/ir.model.access.csv',
     ],
-    "installable": True,
-    "application": True,
-    "auto_install": False
+
+    'installable': True,
+
+    'application': True,
+
+    'auto_install': False
 }
